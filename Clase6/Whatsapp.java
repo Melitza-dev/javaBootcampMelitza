24e4df815c52218a98a64fd4ed7ec1e685312b5d
Melitza-dev <melitza.ortiz.gomez@gmail.com>
2023-06-13 21:29:26 +0200

    Enlace llamadas Whatsapp

---
+++ Clase6/Whatsapp.java
@@ -0,0 +1,21 @@
+package Clase6;
+
+import java.util.Scanner;
+public class Whatsapp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número de telefono sin espacios: ");
+        String telefono = scanner.nextLine();
+
+        String url = "https://api.whatsapp.com/send?phone=" + telefono;
+
+        System.out.println("Puedes llamar por Whatsapp pinchando aqui " + url);
+
+    }
+
+
+}
+
