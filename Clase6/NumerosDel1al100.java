a97b076a5c65e2084153b27b95babb44ea27f146
Melitza-dev <melitza.ortiz.gomez@gmail.com>
2023-06-15 09:39:22 +0200

    Mayor a Menor numeros

---
+++ Clase6/NumerosDel1al100.java
@@ -0,0 +1,13 @@
+package Clase6;
+
+
+public class NumerosDel1al100 {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <= 100) {
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
